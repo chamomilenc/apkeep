7d46d955cc77391ae52f350fac4c35c613ea8c17
liminglong <chamomileyx@163.com>
2026-09-03 15:27:56 +0800

    实验脚本1

---
+++ all.sh
@@ -0,0 +1,6 @@
+
+nohup bash brust.sh > logs/stdout.log 2>&1 &
+
+nohup bash incr.sh > logs/stdout.log 2>&1 &
+
+nohup bash exp-5.sh > logs/stdout.log 2>&1 &
---
+++ exp-5.sh
@@ -0,0 +1,30 @@
+
+java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/B4-2013/experiment-inputs/incremental-u10000-profile-forwarding-only --verify invariants
+java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/B4-2018/experiment-inputs/incremental-u10000-profile-forwarding-only --verify invariants
+java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/BtNorthAmerica/experiment-inputs/incremental-u10000-profile-forwarding-only --verify invariants
+java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/Ntt/experiment-inputs/incremental-u10000-profile-forwarding-only --verify invariants
+java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/Oteglobe/experiment-inputs/incremental-u10000-profile-forwarding-only --verify invariants
+java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/airtel1/experiment-inputs/incremental-u10000-profile-forwarding-only --verify invariants
+java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/airtel2/experiment-inputs/incremental-u10000-profile-forwarding-only --verify invariants
+java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/berkley-ribs-add-random-remove-random/experiment-inputs/incremental-u10000-profile-forwarding-only --verify invariants
+java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/i2/experiment-inputs/incremental-u10000-profile-forwarding-only --verify invariants
+java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/inet/experiment-inputs/incremental-u10000-profile-forwarding-only --verify invariants
+java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/rf1755/experiment-inputs/incremental-u10000-profile-forwarding-only --verify invariants
+java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/rf3257/experiment-inputs/incremental-u10000-profile-forwarding-only --verify invariants
+java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/rf6461/experiment-inputs/incremental-u10000-profile-forwarding-only --verify invariants
+java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/rf6461/experiment-inputs/incremental-u10000-profile-forwarding-only --verify invariants
+
+java -Xmx128g -jar target/apkeep-1.0.0.jar -incr /root/mint/networks/B4-2013/experiment-inputs/incremental-u10000-profile-full
+java -Xmx128g -jar target/apkeep-1.0.0.jar -incr /root/mint/networks/B4-2018/experiment-inputs/incremental-u10000-profile-full
+java -Xmx128g -jar target/apkeep-1.0.0.jar -incr /root/mint/networks/BtNorthAmerica/experiment-inputs/incremental-u10000-profile-full
+java -Xmx128g -jar target/apkeep-1.0.0.jar -incr /root/mint/networks/Ntt/experiment-inputs/incremental-u10000-profile-full
+java -Xmx128g -jar target/apkeep-1.0.0.jar -incr /root/mint/networks/Oteglobe/experiment-inputs/incremental-u10000-profile-full
+java -Xmx128g -jar target/apkeep-1.0.0.jar -incr /root/mint/networks/airtel1/experiment-inputs/incremental-u10000-profile-full
+java -Xmx128g -jar target/apkeep-1.0.0.jar -incr /root/mint/networks/airtel2/experiment-inputs/incremental-u10000-profile-full
+java -Xmx128g -jar target/apkeep-1.0.0.jar -incr /root/mint/networks/berkley-ribs-add-random-remove-random/experiment-inputs/incremental-u10000-profile-full
+java -Xmx128g -jar target/apkeep-1.0.0.jar -incr /root/mint/networks/i2/experiment-inputs/incremental-u10000-profile-full
+java -Xmx128g -jar target/apkeep-1.0.0.jar -incr /root/mint/networks/inet/experiment-inputs/incremental-u10000-profile-full
+java -Xmx128g -jar target/apkeep-1.0.0.jar -incr /root/mint/networks/rf1755/experiment-inputs/incremental-u10000-profile-full
+java -Xmx128g -jar target/apkeep-1.0.0.jar -incr /root/mint/networks/rf3257/experiment-inputs/incremental-u10000-profile-full
+java -Xmx128g -jar target/apkeep-1.0.0.jar -incr /root/mint/networks/rf6461/experiment-inputs/incremental-u10000-profile-full
+java -Xmx128g -jar target/apkeep-1.0.0.jar -incr /root/mint/networks/rf6461/experiment-inputs/incremental-u10000-profile-full
