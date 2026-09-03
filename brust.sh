ed9cc21ca1897646afea75f8651438b848d8a60a
liminglong <chamomileyx@163.com>
2026-09-03 15:18:37 +0800

    实验脚本1

--- brust.sh
+++ brust.sh
@@ -1,18 +1,18 @@
 
-java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/B4-2013/experiment-inputs/burst-u10000-profile-full --verify reachability
-java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/B4-2018/experiment-inputs/burst-u100000-profile-full --verify reachability
-java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/BtNorthAmerica/experiment-inputs/burst-u100000-profile-full --verify reachability
-java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/Ntt/experiment-inputs/burst-u100000-profile-full --verify reachability
-java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/Oteglobe/experiment-inputs/burst-u100000-profile-full --verify reachability
-java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/airtel1/experiment-inputs/burst-u10000-profile-full --verify reachability
-java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/airtel2/experiment-inputs/burst-u10000-profile-full --verify reachability
-java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/berkley-ribs-add-random-remove-random/experiment-inputs/burst-u100000-profile-full --verify reachability
-java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/i2/experiment-inputs/burst-u10000-profile-full --verify reachability
-java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/inet/experiment-inputs/burst-u100000-profile-full --verify reachability
-java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/rf1755/experiment-inputs/burst-u100000-profile-full --verify reachability
-java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/rf3257/experiment-inputs/burst-u100000-profile-full --verify reachability
-java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/rf6461/experiment-inputs/burst-u100000-profile-full --verify reachability
-java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/rf6461/experiment-inputs/burst-u3000-profile-full --verify reachability
+java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/B4-2013/experiment-inputs/burst-u10000-q10000-profile-full --verify reachability
+java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/B4-2018/experiment-inputs/burst-u100000-q10000-profile-full --verify reachability
+java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/BtNorthAmerica/experiment-inputs/burst-u100000-q10000-profile-full --verify reachability
+java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/Ntt/experiment-inputs/burst-u100000-q10000-profile-full --verify reachability
+java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/Oteglobe/experiment-inputs/burst-u100000-q10000-profile-full --verify reachability
+java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/airtel1/experiment-inputs/burst-u10000-q10000-profile-full --verify reachability
+java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/airtel2/experiment-inputs/burst-u10000-q10000-profile-full --verify reachability
+java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/berkley-ribs-add-random-remove-random/experiment-inputs/burst-u100000-q10000-profile-full --verify reachability
+java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/i2/experiment-inputs/burst-u10000-q10000-profile-full --verify reachability
+java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/inet/experiment-inputs/burst-u100000-q10000-profile-full --verify reachability
+java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/rf1755/experiment-inputs/burst-u100000-q10000-profile-full --verify reachability
+java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/rf3257/experiment-inputs/burst-u100000-q10000-profile-full --verify reachability
+java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/rf6461/experiment-inputs/burst-u100000-q10000-profile-full --verify reachability
+java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/mint/networks/rf6461/experiment-inputs/burst-u3000-q10000-profile-full --verify reachability
 
 
 
