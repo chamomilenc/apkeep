0759c112751ca7da4b954753b00a21959edd90b3
liminglong <chamomileyx@163.com>
2026-09-04 10:15:47 +0800

    all.sh

--- all.sh
+++ all.sh
@@ -1,6 +1,10 @@
 
 nohup bash brust.sh > logs/stdout.log 2>&1 &
 
+nohup bash brust-fo.sh > logs/stdout.log 2>&1 &
+
 nohup bash incr.sh > logs/stdout.log 2>&1 &
+
+nohup bash incr-fo.sh > logs/stdout.log 2>&1 &
 
 nohup bash exp-5.sh > logs/stdout.log 2>&1 &
