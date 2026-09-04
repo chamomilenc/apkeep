69049b3813bf86ebd46902db97913ec90b437057
liminglong <chamomileyx@163.com>
2026-09-04 10:55:23 +0800

    实验6

--- all.sh
+++ all.sh
@@ -8,3 +8,5 @@ nohup bash incr.sh > logs/stdout.log 2>&1 &
 nohup bash incr-fo.sh > logs/stdout.log 2>&1 &
 
 nohup bash exp-5.sh > logs/stdout.log 2>&1 &
+
+nohup bash exp-6.sh > logs/stdout.log 2>&1 &
---
+++ exp-6.sh
@@ -0,0 +1,11 @@
+
+
+java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/apkeep/networks/stanford --verify invariants
+
+java -Xmx128g -jar target/apkeep-1.0.0.jar -burst /root/apkeep/networks/stanford-noacl --verify invariants
+
+java -Xmx128g -jar target/apkeep-1.0.0.jar -incr /root/apkeep/networks/stanford --verify invariants
+
+java -Xmx128g -jar target/apkeep-1.0.0.jar -incr /root/apkeep/networks/stanford-noacl --verify invariants
+
+
