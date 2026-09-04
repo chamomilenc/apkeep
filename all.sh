831b9da3a084b97e43b87222bd8ef6ab3908f014
liminglong <chamomileyx@163.com>
2026-09-04 16:32:10 +0800

    BDD操作时间对比 sh

--- all.sh
+++ all.sh
@@ -10,3 +10,5 @@ nohup bash incr-fo.sh > logs/stdout.log 2>&1 &
 nohup bash exp-5.sh > logs/stdout.log 2>&1 &
 
 nohup bash exp-6.sh > logs/stdout.log 2>&1 &
+
+nohup bash bdd.sh > logs/stdout.log 2>&1 &
---
+++ bdd.sh
@@ -0,0 +1,5 @@
+
+
+java -Xmx128g -jar target/apkeep-1.0.0.jar -bdd /root/apkeep/networks/stanford
+
+java -Xmx128g -jar target/apkeep-1.0.0.jar -bdd /root/apkeep/networks/stanford-noacl
